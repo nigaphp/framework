e21db446e67bda2c21b4959d9c5dd4338fb2ee1a
Abass Ben Cheik <cheikabassben@gmail.com>
2021-09-02 20:01:10 +0000

    Feat(bundle): add framework bundle features

---
+++ Controller/Controller.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * This file is part of the Nigatedev PHP framework package
+ *
+ *  (c) Abass Ben Cheik <abass@todaysdev.com>
+ */
+   
+namespace Nigatedev\FrameworkBundle\Controller;
+
+use Nigatedev\FrameworkBundle\Application\App;
+
+/**
+ * App core controller class
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+class Controller
+{
+    
+    /**
+     * @param string $view
+     * @param string[] $params
+     *
+     * @return mixed
+     */
+    public function render(string $view, array $params = [])
+    {
+        return App::$app->diyan->render($view, $params);
+    }
+}
