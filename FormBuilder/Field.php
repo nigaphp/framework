c41aeeab907e1db013ae4e1839a71e8dd3770554
Abass Ben Cheik <cheikabassben@gmail.com>
2021-10-12 15:57:40 +0000

    feat(formBluider): add html form builder features

---
+++ FormBuilder/Field.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * This file is part of the Nigatedev PHP framework package
+ *
+ * (c) Abass Ben Cheik <abass@todaysdev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types = 1);
+ 
+namespace Nigatedev\FrameworkBundle\FormBuilder;
+
+use Nigatedev\FrameworkBundle\Models\AbstractModel;
+
+/**
+ * Form fields
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+class Field
+{
+    public const TYPE_TEXT = "text";
+    public const TYPE_EMAIL = "email";
+    public const TYPE_PASSWORD = "password";
+    public const TYPE_NUMBER = "number";
+    public const TYPE_CHECKBOX = "checkbox";
+    public const TYPE_RADIOBOX = "radiobox";
+    public string $type;
+    public AbstractModel $model;
+    public string $attribute;
+    
+    /**
+     * Field constructor
+     */
+    public function __construct(AbstractModel $model, string $attribute)
+    {
+        $this->model = $model;
+        $this->attribute = $attribute;
+        $this->type = self::TYPE_TEXT;
+    }
+    
+    public function __toString()
+    {
+        
+        return sprintf('
+            <div class="form-group">
+                <label for="%s" class="form-label">%s</label>
+                <input name="%s" value="%s" type="%s" id="%s" class="form-control%s">
+                <div class="text-danger invalide-feedback">%s</div>
+            </div>',
+            $this->attribute,
+            $this->attribute,
+            $this->attribute,
+            $this->model->{$this->attribute},
+            $this->type,
+            $this->attribute,
+            $this->model->hasError($this->attribute) ? " is-invalid" : "",
+            $this->model->getFirstError($this->attribute)
+        );
+    }
+    
+    /**
+     * HTML input type password
+     * 
+     * @return self
+     */
+    public function passwordField(): self
+    {
+        $this->type = self::TYPE_PASSWORD;
+        return $this;
+    }
+    
+    /**
+     * HTML input type email
+     * 
+     * @return self
+     */
+    public function emailField(): self
+    {
+        $this->type = self::TYPE_EMAIL;
+        return $this;
+    }
+    
+    /**
+     * HTML input type checkbox
+     * 
+     * @return self
+     */
+    public function checkboxField(): self
+    {
+        $this->type = self::TYPE_CHECKBOX;
+        return $this;
+    }
+    
+    /**
+     * HTML input type radiobox
+     * 
+     * @return self
+     */
+    public function radioboxField(): self
+    {
+        $this->type = self::TYPE_RADIOBOX;
+        return $this;
+    }
+}
---
+++ FormBuilder/Form.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * This file is part of the Nigatedev PHP framework package
+ *
+ * (c) Abass Ben Cheik <abass@todaysdev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types = 1);
+ 
+namespace Nigatedev\FrameworkBundle\FormBuilder;
+
+use Nigatedev\FrameworkBundle\Models\AbstractModel;
+
+/**
+ * Main Form class
+ *
+ * @author Abass Ben Cheik <abass@todaysdev.com>
+ */
+class Form
+{
+    /**
+     * The opening tag of the form (e.g: <form>)
+     * @param string $action     The action
+     * @param string $method     The method get|post|delete...
+     *
+     * @return Form
+     */
+    public static function formStart(string $action = "", string $method = "get")
+    {
+        echo sprintf('<form action="%s" method="%s" class="g-3 mb-4 mt-4">', $action, $method);
+    
+        return new Form();
+    }
+    
+    /**
+     * The closing tag of the form     (e.g: </form>)
+     *
+     * @return string
+     */
+    public static function formEnd()
+    {
+        echo '</form>';
+    }
+    
+    /**
+     * Input field
+     *
+     * @param AbstractModel $model
+     * @param string $attribute
+     *
+     * @return Field
+     */
+    public function field(AbstractModel $model, $attribute)
+    {
+        return new Field($model, $attribute);
+    }
+}
